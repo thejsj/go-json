a130824a6e96f7c862ae1589b3fe5fff3b58c294
thejsj <jorge.silva@thejsj.com>
2015-10-10 15:41:40 -0700

    Initial Commit

---
+++ main.go
@@ -0,0 +1,146 @@
+package main;
+
+import (
+    "io/ioutil"
+    "strings"
+    "strconv"
+    "log"
+    "os"
+)
+
+func FirstAndLastChars (first string, last string) func (str string) bool {
+    return func (str string) bool {
+        return string(str[0]) == first && str[len(str)-1:] == last
+    }
+}
+
+func SplitByChar (baseChar string) func (str string) []string {
+    return func (str string) []string {
+        var result []string;
+        doubleStringOpen := true
+        singleStringOpen := true
+        arrayOpen := false
+        objectOpen := false
+        arrayBracketCount := 0
+        objectBracketCount := 0
+        currStr := ""
+        prevCh := ""
+        for i := 0; i < len(str); i++ {
+            ch := string(str[i])
+            if ch == "\"" {
+                doubleStringOpen := !doubleStringOpen
+            }
+            if ch == "'" {
+                singleStringOpen := !singleStringOpen
+            }
+            if ch == "[" {
+                arrayBracketCount += 1
+                arrayOpen = true
+            }
+            if ch == "]" {
+                arrayBracketCount -= 1
+                if arrayBracketCount == 0 {
+                    arrayOpen = false
+                }
+            }
+            if ch == "{" {
+                objectBracketCount += 1
+                objectOpen = true
+            }
+            if ch == "}" {
+                objectBracketCount -= 1
+                if objectBracketCount == 0 {
+                    objectOpen = false
+                }
+            }
+            if
+                ch == baseChar && 
+                !doubleStringOpen && 
+                !singleStringOpen && 
+                !arrayOpen&& 
+                !objectOpen {
+                if currStr != ""  {
+                    appendedStr := strings.TrimSpace(currStr)
+                    result := append(result, appendedStr)
+                }
+                currStr = "";
+                prevCh = "";
+            } else {
+              currStr += ch;
+              prevCh = ch;
+            }
+        }
+        if currStr != "" {
+            appendedStr := strings.TrimSpace(currStr)
+            result = append(result, appendedStr)
+        }
+        return result
+    }
+}
+
+func ParseJSON (str string) string {
+    IsArray := FirstAndLastChars("[", "]")
+    IsObj := FirstAndLastChars("{", "}")
+    HasDoubleQuotes := FirstAndLastChars("\"", "\"")
+    HasSingleQuotes := FirstAndLastChars("''", "''")
+    IsString := func (str string) bool {
+        // Do I need to only trim space or is there something else
+        // that need to get trimmed?
+        str = strings.TrimSpace(str)
+        return (HasSingleQuotes(str) || HasDoubleQuotes(str)) && str[len(str)-2:] != "\\";
+    }
+    IsNumber := func (str string) bool {
+        floatValue, err := strconv.ParseFloat(str, 64)
+        return string(floatValue) == str
+    }
+    RemoveFirstAndLastChar := func (str string) string {
+        // Do I need to only trim space or is there something else
+        // that need to get trimmed?
+        str = strings.TrimSpace(str)
+        return str[1:len(str) - 1]
+    }
+    SeparateStringByCommans := SplitByChar(",")
+    SeparateStringByColons := SplitByChar(":")
+
+    str = strings.TrimSpace(str)
+    if IsArray (str) {
+        var arrParts []interface{}
+        strParts := (RemoveFirstAndLastChar(str))
+        for i := 0; i < len(strParts); i++ {
+            arrParts = append(arrParts, ParseJSON(strParts[i]))
+        }
+        return strParts
+    }
+    if IsObj(str) {
+        return true
+    }
+    if IsString (str) {
+        return RemoveFirstAndLastChar(str)
+    }
+    if IsNumber (str) {
+        return strconv.ParseFloat(str, 64)
+    }
+    if str == "nil" {
+        return nil
+    }
+    if str == "false" {
+        return false
+    }
+    if str == "true" {
+        return true
+    }
+    parsedJSONString := ""
+    return parsedJSONString
+}
+
+func main() {
+    bytes, err := ioutil.ReadAll(os.Stdin)
+    inputString := string(bytes)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    parsedJSONString := ParseJSON(inputString)
+    log.Println(parsedJSONString)
+}
